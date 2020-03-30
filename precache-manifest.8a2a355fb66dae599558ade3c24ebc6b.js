32a1a9f150b45908b1631bdb21689b5e3e09ed93
divy-saxena95 <divysaxena2@gmail.com>
2020-03-30 13:48:28 +0530

    Updates

---
+++ precache-manifest.8a2a355fb66dae599558ade3c24ebc6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826fb605952b95c66669c4c0cc933449",
+    "url": "/bluestacksassignment/index.html"
+  },
+  {
+    "revision": "43497ebdf6154c55d465",
+    "url": "/bluestacksassignment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82dfe7d2d98ff8118fe4",
+    "url": "/bluestacksassignment/static/js/2.a03302b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bluestacksassignment/static/js/2.a03302b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43497ebdf6154c55d465",
+    "url": "/bluestacksassignment/static/js/main.400afc1e.chunk.js"
+  },
+  {
+    "revision": "1da27b8345d96efe4301",
+    "url": "/bluestacksassignment/static/js/runtime-main.c6547d22.js"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/bluestacksassignment/static/media/Auto_chess.977ecf7b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bluestacksassignment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "688d7dd465a4804407797e5e427f564b",
+    "url": "/bluestacksassignment/static/media/logo.688d7dd4.png"
+  }
+]);
